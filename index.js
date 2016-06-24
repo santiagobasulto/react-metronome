79350084af5313bc7ff9b4ff58b3e8bf9caed542
Santiago Basulto <santiago.basulto@gmail.com>
2016-06-24 14:27:15 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom'
+require('./screen.scss');
+
+render(
+  <h1>Hello World</h1>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: false,
+    port: 3333
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  }
+};
